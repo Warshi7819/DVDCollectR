8cb93af52094281b54d688a8bb87fb2cf7e68dc3
Rune Devik <runedevik@yahoo.no>
2026-07-17 10:41:42 +0200

    Adding possibility of viewing the cover  images full size as an overlay of the details page.

--- DVDCollectRWeb/wwwroot/js/site.js
+++ DVDCollectRWeb/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.clickable-cover').forEach(function (img) {
+    img.addEventListener('click', function () {
+      document.getElementById('coverModalImage').src = this.src;
+      var modal = new bootstrap.Modal(document.getElementById('coverModal'));
+      modal.show();
+    });
+  });
+});
